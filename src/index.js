90fa6b73740213bef63c5a489f14b2a24314bcdb
Lance Dang <lancedang64@gmail.com>
2021-05-10 04:02:17 +1000

    Updated README

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@
 
 // import domController from './modules/domCreator.js';
 // import itemFactory from './modules/item.js';
-import eventHandler from './modules/eventHandler.js';
+import eventsHandler from './modules/eventsHandler.js';
 
 // const myItem = itemFactory('Learn Shortcuts','James website', '1/6/21', 1)
 // const myDomItem = domController.getItem(myItem);
 // document.querySelector('.content').appendChild(myDomItem);
-eventHandler.addEventListeners();
-    
+eventsHandler.addListenersOnPageLoad();
