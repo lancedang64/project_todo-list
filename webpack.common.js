c4bdcfb6c48fe1cfc11e6ef70e4f51a450dc3552
Lance Dang <lancedang64@gmail.com>
2021-05-15 18:29:12 +1000

    Bundled src for production

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
+  entry: {
+    app: './src/index.js',
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
